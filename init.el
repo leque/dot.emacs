191ff0695cea9ac5e576de20a970314c9e1522c6
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-01-01 15:58:39 +0900

    use executable-find instead of locate-file

--- init.el
+++ init.el
@@ -255,19 +255,19 @@ otherwise displays a warnning message and returns nil."
   )
 
 ;; Add opam emacs directory to the load-path
-(when (locate-file "opam" exec-path)
+(when (executable-find "opam")
   (setq opam-share
         (substring
          (shell-command-to-string "opam config var share 2> /dev/null")
          0
          -1))
-  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp")))
-
-(progn
-  (autoload 'merlin-mode "merlin" "Merlin mode" t)
-  (add-hook 'caml-mode-hook 'merlin-mode t)
-  (setq merlin-use-auto-complete-mode 'easy)
-  (setq merlin-command 'opam))
+  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
+  (when (locate-library "merlin")
+    (autoload 'merlin-mode "merlin" "Merlin mode" t)
+    (add-hook 'caml-mode-hook 'merlin-mode t)
+    (setq merlin-use-auto-complete-mode 'easy)
+    (setq merlin-command 'opam))
+  )
 
 (el-get-bundle sml-mode
   (with-eval-after-load-feature 'sml-mode
