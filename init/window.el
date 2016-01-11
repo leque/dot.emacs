f4d0db02608014abea1913b75ef3664c59e88e40
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-01-12 01:16:09 +0900

    window manipulation

---
+++ init/window.el
@@ -0,0 +1,15 @@
+(global-set-key (kbd "s-h") 'windmove-right)
+(global-set-key (kbd "s-j") 'windmove-down)
+(global-set-key (kbd "s-k") 'windmove-up)
+(global-set-key (kbd "s-l") 'windmove-left)
+
+(global-set-key (kbd "s-H") 'shrink-window-horizontally)
+(global-set-key (kbd "s-J") 'shrink-window)
+(global-set-key (kbd "s-K") 'enlarge-window)
+(global-set-key (kbd "s-L") 'enlarge-window-horizontally)
+
+(el-get-bundle! buffer-move
+  (global-set-key (kbd "M-g h") 'buf-move-left)
+  (global-set-key (kbd "M-g j") 'buf-move-down)
+  (global-set-key (kbd "M-g k") 'buf-move-up)
+  (global-set-key (kbd "M-g l") 'buf-move-right))
