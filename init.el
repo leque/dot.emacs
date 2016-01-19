24037986af85600be5b82aeb03143466930e957f
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-01-19 23:21:31 +0900

    do not start auto-complete automatically

--- init.el
+++ init.el
@@ -98,6 +98,7 @@ otherwise displays a warnning message and returns nil."
 ;;; auto-complete
 (el-get-bundle! auto-complete
   (ac-config-default)
+  (setq ac-auto-start nil)
   (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
   )
 
