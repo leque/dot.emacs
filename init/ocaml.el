6a150c77345442520a2e49e1dc14460454643c5c
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-05-05 16:24:34 +0900

    opam-config function

--- init/ocaml.el
+++ init/ocaml.el
@@ -6,16 +6,27 @@
         auto-mode-alist)
   )
 
-(when (executable-find "opam")
-  (defvar opam-share
-    (substring
-     (shell-command-to-string "opam config var share 2> /dev/null")
-     0
-     -1))
-  (add-to-list 'load-path (concat opam-share "/emacs/site-lisp"))
-  (when (locate-library "merlin")
-    (autoload 'merlin-mode "merlin" "Merlin mode" t)
-    (add-hook 'caml-mode-hook 'merlin-mode t)
-    (setq-default merlin-use-auto-complete-mode 'easy)
-    (setq-default merlin-command 'opam))
-  )
+(defvar opam-share nil)
+
+(defun opam-site-lisp ()
+  (and opam-share
+       (concat opam-share "/emacs/site-lisp")))
+
+(defun opam-config ()
+  (interactive)
+  (setq load-path (delete (opam-site-lisp) load-path))
+  (when (executable-find "opam")
+    (setq opam-share
+          (substring
+           (shell-command-to-string "opam config var share 2> /dev/null")
+           0
+           -1))
+    (add-to-list 'load-path (opam-site-lisp))))
+
+(opam-config)
+
+(when (locate-library "merlin")
+  (autoload 'merlin-mode "merlin" "Merlin mode" t)
+  (add-hook 'caml-mode-hook 'merlin-mode t)
+  (setq-default merlin-use-auto-complete-mode 'easy)
+  (setq-default merlin-command 'opam))
