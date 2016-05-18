bae7c8649026b1c37d90acc505b5b9b3c8b1ed56
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-05-18 14:07:38 +0900

    activate irony-mode and rtags only in supported modes

--- init/c.el
+++ init/c.el
@@ -15,23 +15,27 @@
                   (statement-case-open . 4))))
   )
 
-(el-get-bundle irony-mode
-  (add-hook 'c-mode-common-hook 'flycheck-mode)
-  (add-hook 'c-mode-common-hook 'irony-mode)
-  (with-eval-after-load-feature 'irony
-    (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-    (el-get-bundle! ac-irony
-      (add-to-list 'ac-sources 'ac-source-irony))
-    (el-get-bundle! flycheck-irony
-      (flycheck-irony-setup))
-    ))
+(let ((hooks '(c-mode-hook c++-mode-hook objc-mode-hook)))
+  (el-get-bundle irony-mode
+    (dolist (hook hooks)
+      (add-hook hook 'flycheck-mode)
+      (add-hook hook 'irony-mode))
+    (with-eval-after-load-feature 'irony
+      (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+      (el-get-bundle! ac-irony
+        (add-to-list 'ac-sources 'ac-source-irony))
+      (el-get-bundle! flycheck-irony
+        (flycheck-irony-setup))
+      ))
 
-(el-get-bundle rtags
-  (with-eval-after-load-feature 'rtags
-    (add-hook 'c-mode-common-hook
-              (lambda ()
-                (when (rtags-is-indexed)
-                  (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
-                  (local-set-key (kbd "M-;") 'rtags-find-symbol)
-                  (local-set-key (kbd "M-@") 'rtags-find-references)
-                  (local-set-key (kbd "M-,") 'rtags-location-stack-back))))))
+  (el-get-bundle rtags
+    (with-eval-after-load-feature 'rtags
+      (dolist (hook hooks)
+        (add-hook hook
+                  (lambda ()
+                    (when (rtags-is-indexed)
+                      (local-set-key (kbd "M-.") 'rtags-find-symbol-at-point)
+                      (local-set-key (kbd "M-;") 'rtags-find-symbol)
+                      (local-set-key (kbd "M-@") 'rtags-find-references)
+                      (local-set-key (kbd "M-,") 'rtags-location-stack-back)))))
+      )))
