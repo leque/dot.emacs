b4b40b18e8533ab5b74aa053eb67db3d474a11b3
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-06-22 05:29:26 +0900

    fix typo

--- init/window.el
+++ init/window.el
@@ -1,7 +1,7 @@
-(global-set-key (kbd "s-h") 'windmove-right)
+(global-set-key (kbd "s-h") 'windmove-left)
 (global-set-key (kbd "s-j") 'windmove-down)
 (global-set-key (kbd "s-k") 'windmove-up)
-(global-set-key (kbd "s-l") 'windmove-left)
+(global-set-key (kbd "s-l") 'windmove-right)
 
 (global-set-key (kbd "s-H") 'shrink-window-horizontally)
 (global-set-key (kbd "s-J") 'shrink-window)
