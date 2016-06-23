2a6d729d31f8ab827bba757184639b49353f4405
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-06-24 01:15:28 +0900

    tweak copyright style

--- init/misc.el
+++ init/misc.el
@@ -1,4 +1,12 @@
-(el-get-bundle lice)
+(el-get-bundle lice
+  (with-eval-after-load-feature 'lice
+    (defadvice lice:insert-copyright (around
+                                      override-lice:insert-copyright
+                                      activate)
+      (insert (format "Copyright (c) %s %s\n\n"
+                      (format-time-string "%Y")
+                      (user-full-name))))
+    ))
 
 (defun change-frame-width (w)
   (interactive "nnew width: ")
