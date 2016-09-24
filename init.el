be0e7c235c1e9f4a4d5d7f0f376875b6adfaae47
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2016-09-25 01:20:09 +0900

    add dummy `(package-initialize)`

--- init.el
+++ init.el
@@ -1,4 +1,12 @@
 ;; -*- coding: utf-8-unix -*-
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+
+;; (package-initialize)
+
 (require 'cl-lib)
 
 (when load-file-name
