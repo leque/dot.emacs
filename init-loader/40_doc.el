70e9ec7b64bb3f74faeeacdb268041687436de02
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2018-10-20 03:47:54 +0900

    error handling

--- init-loader/40_doc.el
+++ init-loader/40_doc.el
@@ -2,19 +2,31 @@
   "Convert contents of BUFFER to HTML by using github-markup
 \(URL `https://github.com/github/markup')."
   (let* ((filename (buffer-file-name buffer))
-         st
-         (output (with-output-to-string
-                   (setq st
-                         (call-process "github-markup"
-                                       nil
-                                       standard-output
-                                       nil
-                                       (buffer-file-name buffer))))))
+         status
+         stderr
+         error
+         (output (let ((err-file (make-temp-file "github-markup-err")))
+                   (unwind-protect
+                       (condition-case err
+                           (with-output-to-string
+                             (setq status
+                                   (call-process "github-markup"
+                                                 nil
+                                                 (list standard-output err-file)
+                                                 nil
+                                                 filename))
+                             (setq stderr
+                                   (with-temp-buffer
+                                     (insert-file-contents err-file)
+                                     (buffer-string))))
+                         (error
+                          (setq error err)
+                          nil))
+                     (delete-file err-file)))))
     (insert "<html>")
     (insert (xmlgen
              `(head
-               (title ,(or (buffer-file-name buffer)
-                           (buffer-name buffer)))
+               (title ,(or filename (buffer-name buffer)))
                ;; https://github.com/sindresorhus/github-markdown-css
                ;; https://cdnjs.com/libraries/github-markdown-css
                (link :rel "stylesheet"
@@ -22,24 +34,31 @@
                      :href "https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/2.10.0/github-markdown.min.css")
                (style :type "text/css"
                       "\
-.markdown-body {
-   box-sizing: border-box;
-   min-width: 200px;
-   max-width: 980px;
-   margin: 0 auto;
-   padding: 45px;
+.markdown-body, .error, .stderr {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+}
+.error {
+  color : red;
+  font-weight : bold;
+}
+.stderr {
+  border-bottom : 1px solid #ccc;
 }
 ")
                )))
     (insert "<body>")
-    (cond ((eql st 0)
-           (insert "<div class=\"markdown-body\">")
-           (insert output)
-           (insert "</div>"))
-          (t
-           (insert "<pre>")
-           (insert output)
-           (insert "</pre>")))
+    (when error
+      (insert (xmlgen `(pre :class "error" ,(format "%S" error)))))
+    (when (s-present? stderr)
+      (insert (xmlgen `(pre :class "stderr" ,stderr))))
+    (when (eql status 0)
+      (insert "<div class=\"markdown-body\">")
+      (insert output)
+      (insert "</div>"))
     (insert "</body></html>")))
 
 (add-to-list 'auto-mode-alist
