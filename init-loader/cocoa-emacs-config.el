8176a466e378d143201e491c8c289fb5d8098533
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2020-06-14 22:16:09 +0900

    use UniHentaiKana font on macOS.

--- init-loader/cocoa-emacs-config.el
+++ init-loader/cocoa-emacs-config.el
@@ -3,7 +3,8 @@
 
 (let ((size 14)
       (ascii-font "Monaco")
-      (ja-font "Hiragino Maru Gothic Pro"))
+      (ja-font "Hiragino Maru Gothic Pro")
+      (hg-font "UniHentaiKana"))
   (set-face-attribute 'default nil
                       :family ascii-font
                       :height (* size 10))
@@ -15,6 +16,10 @@
           japanese-jisx0213-1
           japanese-jisx0213-2
           ))
+  (set-fontset-font t
+                    (cons (decode-char 'ucs #x1b001)
+                          (decode-char 'ucs #x1B11E))
+                    hg-font)
   (set-fontset-font t '(#x0080 . #x024F) ascii-font))
 
 (setq frame-inherited-parameters '(font tool-bar-lines))
