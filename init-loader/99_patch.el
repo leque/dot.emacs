09ac73ca19067d3bf557767c7afb626f13a13bd7
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2020-12-02 18:24:05 +0900

    monkey patch for loadhist-unload-element error

---
+++ init-loader/99_patch.el
@@ -0,0 +1,14 @@
+'(with-eval-after-load-feature 'elp
+   ;; monkey patch to avoid
+   ;; `cl--generic-standard-method-combination: Unsupported qualifiers in function loadhist-unload-element: (:before :extra "elp")`
+  (let ((gf (cl--generic 'loadhist-unload-element)))
+    (setf (cl--generic-method-table gf)
+          (loop for meth in (cl--generic-method-table gf)
+                unless (equal (cl--generic-method-qualifiers meth) '(:before :extra "elp"))
+                collect meth))
+    ;; copied and modified from emacs-lisp/elp.el
+    ;; `:extra NAME` qualifier must place before other qualifiers?
+    (cl-defmethod loadhist-unload-element :extra "elp" :before ((x (head defun)))
+      "Un-instrument before unloading a function."
+      (elp-restore-function (cdr x))))
+  )
