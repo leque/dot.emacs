6f992f18d3084dabd173dfcde7c72a1ffed5b041
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2020-12-13 14:59:26 +0900

    require skk-autoloads

--- init-loader/10_skk.el
+++ init-loader/10_skk.el
@@ -1,6 +1,7 @@
 (setq-default skk-init-file (locate-user-emacs-file ".skk.el"))
 
 (el-get-bundle ddskk
+  (require 'skk-autoloads)
   (global-set-key (kbd "C-x C-j") #'skk-mode))
 
 (el-get-bundle nicola-ddskk)
