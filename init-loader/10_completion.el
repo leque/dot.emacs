314be3da2795f117844efa7c8913d48e3c28d9a8
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2022-04-09 23:29:32 +0900

    fix bad interaction between el-get and swiper
    I don't know why this is a fix, though

--- init-loader/10_completion.el
+++ init-loader/10_completion.el
@@ -12,15 +12,14 @@
 (el-get-bundle! flx)
 
 (el-get-bundle! swiper
-  :depends (cl-lib avy hydra)
-  (ivy-mode +1)
-  (global-set-key (kbd "M-x") #'counsel-M-x)
-  (global-set-key (kbd "C-x C-f") #'counsel-find-file)
-  (with-eval-after-load-feature 'ivy
-    (setq ivy-display-style 'fancy)
-    (setq ivy-initial-inputs-alist '())
-    (setq ivy-magic-tilde nil)
-    (setq ivy-re-builders-alist
-          '((t . ivy--regex-fuzzy)))
-    (define-key ivy-minibuffer-map (kbd "TAB") #'ivy-partial)
-    ))
+  :depends (cl-lib avy hydra))
+
+(ivy-mode +1)
+(global-set-key (kbd "M-x") #'counsel-M-x)
+(global-set-key (kbd "C-x C-f") #'counsel-find-file)
+(setq ivy-display-style 'fancy)
+(setq ivy-initial-inputs-alist '())
+(setq ivy-magic-tilde nil)
+(setq ivy-re-builders-alist
+      '((t . ivy--regex-fuzzy)))
+(define-key ivy-minibuffer-map (kbd "TAB") #'ivy-partial)
