04b5b22e8254121d50507a554d07da327f4502e9
OOHASHI Daichi <dico.leque.comicron@gmail.com>
2022-04-09 23:55:19 +0900

    guard with `with-eval-after-load-feature 'shr`

--- init-loader/90_eww.el
+++ init-loader/90_eww.el
@@ -1,3 +1,4 @@
-(defun shr-tag-code (dom)
-  (let ((shr-current-font 'default))
-    (shr-generic dom)))
+(with-eval-after-load-feature 'shr
+  (defun shr-tag-code (dom)
+    (let ((shr-current-font 'default))
+      (shr-generic dom))))
